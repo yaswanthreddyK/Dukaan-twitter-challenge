5b2c8b2453cbbb3d56fda9409fe8206aca77862c
yaswanthreddyK <yaswanthreddy1729@gmail.com>
2024-10-21 19:12:13 +0530

    flowbite-react fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
       <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
       </svg>
-      <input type='text' className='outline-none bg-inherit w-full' placeholder='Search features, tutorials, etc.'/>
+      <input type='text' className='outline-0 bg-inherit w-full border-0 p-0' placeholder='Search features, tutorials, etc.'/>
     </div>
     <div className='text-[#4D4D4D] flex gap-3 items-center sm:flex-grow  justify-end'>
       <div className='bg-[#E6E6E6] p-2 w-8 h-8 sm:w-10 sm:h-10 flex items-center justify-center rounded-full'>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+import flowbite from "flowbite-react/tailwind";
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js"
+    flowbite.content()
   ],
   theme: {
     extend: {
@@ -19,7 +21,7 @@ export default {
     },
   },
   plugins: [
-    import('flowbite/plugin')
+   flowbite.plugin(),
 ]
 }
 
